11533e953babe007f81cca0dfc80bb0cbc6dac65
KNiHT Technologies <knihttechnologies@gmail.com>
2025-10-24 17:06:22 +0300

    fixed styles error

--- src/assets/constants/styles.js
+++ src/assets/constants/styles.js
@@ -1,3 +1,4 @@
+import { SocialLinks, QuickLinks, SocialSVGMap, YouTube, FooterStyles as ComponentStyles } from '../../components/footerparts/Links';
 const generalLightBgColor = 'bg-gradient-to-r from-cyan-200 via-gray-300 to-cyan-200';
 const generalDarkBgColor = 'dark:bg-gradient-to-r dark:from-cyan-900 dark:via-gray-900 dark:to-cyan-900';
 const generalBgColor = `${generalLightBgColor} ${generalDarkBgColor}`;
